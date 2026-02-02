2580fe399495a560ffad5dc958242d2f53597ea3
creativekickstart23 <creativekickstart23@gmail.com>
2026-02-02 20:33:52 +0530

    Update BankSafeApplication.java

--- server/src/main/java/com/edutech/progressive/BankSafeApplication.java
+++ server/src/main/java/com/edutech/progressive/BankSafeApplication.java
@@ -11,34 +11,4 @@ import com.edutech.progressive.dao.AccountDAOImpl;
 import com.edutech.progressive.entity.Accounts;
 @SpringBootApplication
 public class BankSafeApplication {
- 
-    
-
-    public static void main(String[] args) throws Exception{
-        System.out.println("Welcome to BankSafe Project!");
-        AccountDAO accountDAO = new AccountDAOImpl();
- 
-        //List<Accounts> accList = new ArrayList<>();
-        Accounts acc = new Accounts();
-        acc.setCustomerId(101);
-        acc.setBalance(5000);
- 
-        int accountId = accountDAO.addAccount(acc);
- 
-        System.out.println("Inserted Account ID: " + accountId);
- 
-        Accounts inserted = accountDAO.getAccountById(accountId);
-        System.out.println("After insert: "+ inserted);
- 
-        accountDAO.deleteAccount(accountId);
-        System.out.println("Account deleted");
- 
-        Accounts deleted = accountDAO.getAccountById(accountId);
-        System.out.println("After delete: " + deleted);
- 
-        
-
- 
-        
-    }
 }
