c1aca5b8a730e2a66e31f1c4fc3f0119fd2de867
creativekickstart23 <creativekickstart23@gmail.com>
2026-02-03 02:38:56 +0530

    Update BankSafeApplication.java

--- server/src/main/java/com/edutech/progressive/BankSafeApplication.java
+++ server/src/main/java/com/edutech/progressive/BankSafeApplication.java
@@ -1,14 +1,13 @@
 package com.edutech.progressive;
- 
-import java.util.ArrayList;
-import java.util.List;
- 
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
- 
-import com.edutech.progressive.dao.AccountDAO;
-import com.edutech.progressive.dao.AccountDAOImpl;
-import com.edutech.progressive.entity.Accounts;
+
 @SpringBootApplication
 public class BankSafeApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to BankSafe Online!");
+        SpringApplication.run(BankSafeApplication.class, args);
+    }
 }
