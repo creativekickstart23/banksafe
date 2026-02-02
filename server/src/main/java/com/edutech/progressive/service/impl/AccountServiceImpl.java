b9aa754ef279d58c4898fdb3311949e7ff43d07c
creativekickstart23 <creativekickstart23@gmail.com>
2026-02-02 20:29:39 +0530

    Update AccountServiceImpl.java

--- server/src/main/java/com/edutech/progressive/service/impl/AccountServiceImpl.java
+++ server/src/main/java/com/edutech/progressive/service/impl/AccountServiceImpl.java
@@ -1,49 +1,69 @@
 package com.edutech.progressive.service.impl;
-
+ 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
+ 
 import com.edutech.progressive.dao.AccountDAO;
 import com.edutech.progressive.dao.AccountDAOImpl;
 import com.edutech.progressive.entity.Accounts;
 import com.edutech.progressive.service.AccountService;
-
-public class AccountServiceImpl implements AccountService {
-
+ 
+public class AccountServiceImpl implements AccountService{
+ 
     private AccountDAO accountDAO;
-
-    public AccountServiceImpl(AccountDAOImpl accountDAO) {
+    public AccountServiceImpl(AccountDAO accountDAO) {
         this.accountDAO = accountDAO;
     }
-
+ 
+    private static List<Accounts> accountsList = new ArrayList<>();
+ 
     @Override
     public List<Accounts> getAllAccounts() throws SQLException {
         return accountDAO.getAllAccounts();
     }
-
+ 
     @Override
     public int addAccount(Accounts accounts) throws SQLException {
-        return -1;
+ 
+        return accountDAO.addAccount(accounts);
     }
-
+ 
+ 
+    ////////did not understand????  /////done
     @Override
     public List<Accounts> getAllAccountsSortedByBalance() throws SQLException {
-        List<Accounts> accountsList=new ArrayList<>();
-        return accountsList;
+            //List<Accounts> list = accountDAO.getAllAccounts();
+            accountsList = accountDAO.getAllAccounts();
+            Collections.sort(accountsList);
+            return accountsList;
     }
-
-    public List<Accounts> getAccountsByUser(int userId) throws SQLException {
-        return null;
+ 
+ 
+    ////////did not understand???? Incomplete
+    public List<Accounts> getAccountsByUser(int userId) throws SQLException{
+            // accountsList = accountDAO.getAllAccounts();
+            // for(Accounts a : accountsList){
+            //     if(a.g)
+            // }
+ 
+            //return accountDAO.getAllAccountsByCustomers(userId);
+            return accountDAO.getAllAccountsByCustomers(userId);
     }
-
-    public Accounts getAccountById(int accountId) throws SQLException {
-        return null;
+ 
+    public Accounts getAccountById(int accountId) throws SQLException{
+        Accounts account = accountDAO.getAccountById(accountId);
+ 
+        return account;
+       // return accountDAO.getAccountById(accountId);
     }
-    public void updateAccount(Accounts accounts) throws SQLException {
-
+ 
+    public void updateAccount(Accounts accounts)throws SQLException{
+        accountDAO.updateAccount(accounts);
     }
-    public void deleteAccount(int accountId) throws SQLException {
-        
+ 
+    public void deleteAccount(int accountId) throws SQLException{
+        accountDAO.deleteAccount(accountId);
     }
 }
