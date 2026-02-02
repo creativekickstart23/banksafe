8c02a2de6a04cc448998524b6eea7613cf611ce5
creativekickstart23 <creativekickstart23@gmail.com>
2026-02-03 02:27:12 +0530

    Update AccountDAOImpl.java

--- server/src/main/java/com/edutech/progressive/dao/AccountDAOImpl.java
+++ server/src/main/java/com/edutech/progressive/dao/AccountDAOImpl.java
@@ -10,128 +10,88 @@ import java.util.List;
 
 import com.edutech.progressive.config.DatabaseConnectionManager;
 import com.edutech.progressive.entity.Accounts;
+import com.edutech.progressive.service.AccountService;
 
-public class AccountDAOImpl implements AccountDAO {
+public class AccountDAOImpl implements AccountDAO{
+   
+    private Connection connection;
+    public AccountDAOImpl(){
+        try {
+            connection = DatabaseConnectionManager.getConnection();
+        } catch (SQLException e) {
+             e.printStackTrace();
+        }
+    }
+
 
     @Override
     public int addAccount(Accounts accounts) throws SQLException {
-        String sql = "INSERT INTO accounts(customer_id,balance) VALUES(?,?)";
-
-        try (Connection connection1 = DatabaseConnectionManager.getConnection();
-             PreparedStatement ps = connection1.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-
+        String sql="INSERT INTO accounts(customer_id,balance) VALUES(?,?)";
+        PreparedStatement ps = connection.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
             ps.setInt(1, accounts.getCustomerId());
             ps.setDouble(2, accounts.getBalance());
             ps.executeUpdate();
-
             ResultSet rs = ps.getGeneratedKeys();
-            if (rs.next()) {
-                accounts.setAccountId(rs.getInt(1));
+            if(rs.next()){
+                return rs.getInt(1);
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return accounts.getAccountId();
+            return -1;
+
     }
 
     @Override
     public Accounts getAccountById(int accountId) throws SQLException {
-        String sql = "SELECT * FROM accounts WHERE account_id=?";
-
-        try (Connection connection1 = DatabaseConnectionManager.getConnection();
-             PreparedStatement ps = connection1.prepareStatement(sql)) {
-
+        String sql="SELECT * FROM accounts WHERE account_id=?";
+       PreparedStatement ps = connection.prepareStatement(sql);
+     
             ps.setInt(1, accountId);
             ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                Accounts acc = new Accounts();
-                acc.setAccountId(rs.getInt("account_id"));
-                acc.setCustomerId(rs.getInt("customer_id"));
-                acc.setBalance(rs.getDouble("balance"));
-                return acc;
-            } else {
-                return null;
+            if(rs.next()){
+                return new Accounts(rs.getInt("account_id"), rs.getInt("customer_id"), rs.getDouble("balance"));
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
+            return null;
+
+            
+       
     }
 
     @Override
     public void updateAccount(Accounts accounts) throws SQLException {
-        String sql = "UPDATE accounts SET customer_id=?, balance=? WHERE account_id=?";
-
-        try (Connection connection1 = DatabaseConnectionManager.getConnection();
-             PreparedStatement ps = connection1.prepareStatement(sql)) {
-
-            ps.setInt(1, accounts.getCustomerId());
-            ps.setDouble(2, accounts.getBalance());
-            ps.setInt(3, accounts.getAccountId());
+        String sql="UPDATE accounts SET  balance=? WHERE account_id=?";
+        PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setDouble(1, accounts.getBalance());
+            ps.setInt(2, accounts.getAccountId());
             ps.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+            
+      
     }
 
     @Override
     public void deleteAccount(int accountId) throws SQLException {
-        String sql = "DELETE FROM accounts WHERE account_id=?";
-     
-        try (Connection connection1 = DatabaseConnectionManager.getConnection();
-             PreparedStatement ps = connection1.prepareStatement(sql)) {
+        String sql="DELETE FROM accounts WHERE account_id=?";
+        PreparedStatement ps = connection.prepareStatement(sql);
+             ps.setInt(1, accountId);
+              ps.executeUpdate();
+             
 
-            ps.setInt(1, accountId);
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+      
     }
 
     @Override
     public List<Accounts> getAllAccounts() throws SQLException {
-        List<Accounts> accounts = new ArrayList<>();
-        String sql = "SELECT * FROM accounts";
-
-        try (Connection connection1 = DatabaseConnectionManager.getConnection();
-             PreparedStatement ps = connection1.prepareStatement(sql)) {
-
+        List<Accounts> list = new ArrayList<>();
+        String sql="SELECT * FROM accounts";
+        PreparedStatement ps = connection.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                Accounts account = new Accounts(
-                    rs.getInt("account_id"),
-                    rs.getInt("customer_id"),
-                    rs.getDouble("balance")
-                );
-                accounts.add(account);
+            while(rs.next()){
+              Accounts acc =   new Accounts(rs.getInt("account_id"), rs.getInt("customer_id"), rs.getDouble("balance"));
+              list.add(acc);
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return accounts;
+            return list;
+        
     }
-
-    @Override
-    public List<Accounts> getAllAccountsByCustomers(int customer_id) throws SQLException {
-        List<Accounts> accounts = new ArrayList<>();
-        String sql = "SELECT * FROM accounts WHERE customer_id=?";
-
-        try (Connection connection1 = DatabaseConnectionManager.getConnection();
-             PreparedStatement ps = connection1.prepareStatement(sql)) {
-
-            ps.setInt(1, customer_id);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                Accounts account = new Accounts(
-                    rs.getInt("account_id"),
-                    rs.getInt("customer_id"),
-                    rs.getDouble("balance")  
-                );
-                accounts.add(account);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return accounts;
-    }
+    
 }
+
+
+// ___________________________________________ NEW CHANGES DONE__________________________________________
