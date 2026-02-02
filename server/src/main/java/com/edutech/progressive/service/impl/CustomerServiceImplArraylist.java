35dc572e8e312423bc7bde5fd61a9bfe3417eb3a
creativekickstart23 <creativekickstart23@gmail.com>
2026-02-03 02:36:16 +0530

    Update CustomerServiceImplArraylist.java

--- server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImplArraylist.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImplArraylist.java
@@ -2,35 +2,30 @@ package com.edutech.progressive.service.impl;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import com.edutech.progressive.entity.Customers;
-import com.edutech.progressive.service.CustomerService;
 
-public class CustomerServiceImplArraylist implements CustomerService {
-    public static List<Customers> customersList=new ArrayList<>();
+public class CustomerServiceImplArraylist {
 
-    @Override
+    private static List<Customers> customersList = new ArrayList<>();
+
     public List<Customers> getAllCustomers() throws SQLException {
         return customersList;
     }
 
-    @Override
     public int addCustomer(Customers customers) throws SQLException {
         customersList.add(customers);
-        int s=customersList.size();
-        return s;
+        return customersList.size();
     }
 
-    @Override
     public List<Customers> getAllCustomersSortedByName() throws SQLException {
-        Collections.sort(customersList);
+        customersList.sort((a, b) ->
+                a.getName().compareToIgnoreCase(b.getName()));
         return customersList;
     }
 
-    public void emptyArrayList(){
+    public void emptyArrayList() {
         customersList.clear();
     }
-
 }
