5ef2774fdada4c7ec10aab0f0846a65c13fee194
creativekickstart23 <creativekickstart23@gmail.com>
2026-02-05 01:06:42 +0530

    Update AccountRepository.java

--- server/src/main/java/com/edutech/progressive/repository/AccountRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/AccountRepository.java
@@ -1,10 +1,11 @@
 package com.edutech.progressive.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.edutech.progressive.entity.Accounts;
 
-@Repository
 public interface AccountRepository extends JpaRepository<Accounts, Integer> {
+    List<Accounts> findByCustomerId(int customerId);
 }
