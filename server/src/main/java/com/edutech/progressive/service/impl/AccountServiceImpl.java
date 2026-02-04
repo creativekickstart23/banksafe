cd3cd4b182eaed2f1146e08dc8972e9ef70f1b90
creativekickstart23 <creativekickstart23@gmail.com>
2026-02-05 01:14:17 +0530

    Update AccountServiceImpl.java

--- server/src/main/java/com/edutech/progressive/service/impl/AccountServiceImpl.java
+++ server/src/main/java/com/edutech/progressive/service/impl/AccountServiceImpl.java
@@ -1,55 +1,56 @@
 package com.edutech.progressive.service.impl;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.edutech.progressive.dao.AccountDAO;
 import com.edutech.progressive.dao.AccountDAOImpl;
 import com.edutech.progressive.entity.Accounts;
 import com.edutech.progressive.service.AccountService;
 
-public class AccountServiceImpl implements AccountService  {
+@Service
+public class AccountServiceImpl implements AccountService {
+    @Autowired
     private AccountDAO accountDAO;
-    public AccountServiceImpl(AccountDAO accountDAO){
+
+    public AccountServiceImpl(AccountDAOImpl accountDAO) {
         this.accountDAO = accountDAO;
     }
+
     @Override
     public List<Accounts> getAllAccounts() throws SQLException {
-        // TODO Auto-generated method stub
-       return  accountDAO.getAllAccounts();
+        return accountDAO.getAllAccounts();
+    }
+
+    public Accounts getAccountById(int accountId) throws SQLException {
+        return accountDAO.getAccountById(accountId);
     }
 
     @Override
     public int addAccount(Accounts accounts) throws SQLException {
-        // TODO Auto-generated method setDoubl
         return accountDAO.addAccount(accounts);
     }
 
-    @Override
+    public void updateAccount(Accounts accounts) throws SQLException {
+        accountDAO.updateAccount(accounts);
+    }
+
     public void deleteAccount(int accountId) throws SQLException {
         accountDAO.deleteAccount(accountId);
     }
 
     @Override
-    public Accounts getAccountById(int accountId) throws SQLException {
-        return accountDAO.getAccountById(accountId);
-    }
-
-
-    @Override
     public List<Accounts> getAllAccountsSortedByBalance() throws SQLException {
-        List<Accounts> accounts = accountDAO.getAllAccounts();
-       
-        accounts.sort((a,b) -> Double.compare(a.getBalance(), b.getBalance()) );
-        return accounts;
+        List<Accounts> ans = accountDAO.getAllAccounts();
+        Collections.sort(ans);
+        return ans;
     }
 
-    @Override
-    public  void updateAccount(Accounts account) throws SQLException {
-        accountDAO.updateAccount(account);
+    public List<Accounts> getAccountsByUser(int userId) throws SQLException {
+        return accountDAO.getAllAccountsByCustomer(userId);
     }
-
 
 }
