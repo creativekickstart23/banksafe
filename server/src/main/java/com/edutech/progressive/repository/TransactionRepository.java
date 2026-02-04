8ec2e067dcdfbd880093d6b4e02dfd6f9798b878
creativekickstart23 <creativekickstart23@gmail.com>
2026-02-05 01:08:42 +0530

    Day6.java

--- server/src/main/java/com/edutech/progressive/repository/TransactionRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/TransactionRepository.java
@@ -1,10 +1,8 @@
 package com.edutech.progressive.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.edutech.progressive.entity.Transactions;
 
-@Repository
-public interface TransactionRepository extends JpaRepository<Transactions,Integer> {
+public interface TransactionRepository extends JpaRepository<Transactions, Integer> {
 }
