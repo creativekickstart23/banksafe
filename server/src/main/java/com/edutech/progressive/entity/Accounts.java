60edcee63e3b0980f0dd63b70a02af8cc054b85e
creativekickstart23 <creativekickstart23@gmail.com>
2026-02-05 01:02:50 +0530

    Update Accounts.java

--- server/src/main/java/com/edutech/progressive/entity/Accounts.java
+++ server/src/main/java/com/edutech/progressive/entity/Accounts.java
@@ -1,37 +1,72 @@
 package com.edutech.progressive.entity;
 
-public class Accounts implements Comparable<Accounts>{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Accounts implements Comparable<Accounts> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int accountId;
+    // @ManyToOne
+    // @JoinColumn(name = "customerId")
     private int customerId;
     private double balance;
+
     public Accounts() {
     }
+
+    public Accounts(double balance) {
+        this.balance = balance;
+    }
+
+    public Accounts(int customerId, double balance) {
+        this.customerId = customerId;
+        this.balance = balance;
+    }
+
     public Accounts(int accountId, int customerId, double balance) {
         this.accountId = accountId;
         this.customerId = customerId;
         this.balance = balance;
     }
+
     public int getAccountId() {
         return accountId;
     }
+
     public void setAccountId(int accountId) {
         this.accountId = accountId;
     }
+
     public int getCustomerId() {
         return customerId;
     }
+
     public void setCustomerId(int customerId) {
         this.customerId = customerId;
     }
+
     public double getBalance() {
         return balance;
     }
+
     public void setBalance(double balance) {
         this.balance = balance;
     }
-    
+
     @Override
-    public int compareTo(Accounts otherAccounts) {
-        return Double.compare(this.balance, otherAccounts.balance);
+    public int compareTo(Accounts o) {
+        return Double.compare(this.getBalance(), o.getBalance());
     }
+
+    @Override
+    public String toString() {
+        return "Accounts [accountId=" + accountId + ", customerId=" + customerId + ", balance=" + balance + "]";
+    }
+
 }
