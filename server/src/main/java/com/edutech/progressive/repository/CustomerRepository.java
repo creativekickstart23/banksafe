acbb061b3ad2f2107cb4c71df31fd1cec6717175
creativekickstart23 <creativekickstart23@gmail.com>
2026-02-05 01:07:54 +0530

    Update CustomerRepository.java

--- server/src/main/java/com/edutech/progressive/repository/CustomerRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/CustomerRepository.java
@@ -1,13 +1,12 @@
 package com.edutech.progressive.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.edutech.progressive.entity.Customers;
 
-@Repository
-public interface CustomerRepository extends JpaRepository<Customers,Integer> {
-
+public interface CustomerRepository extends JpaRepository<Customers, Integer> {
     Customers findByCustomerId(int customerId);
+
     void deleteByCustomerId(int customerId);
+
 }
