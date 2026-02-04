b8b6a6780fd2f3a274259dfb1801b6c907dd8f9c
creativekickstart23 <creativekickstart23@gmail.com>
2026-02-05 01:16:23 +0530

    Update CustomerServiceImpl.java

--- server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImpl.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImpl.java
@@ -1,52 +1,51 @@
 package com.edutech.progressive.service.impl;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.edutech.progressive.dao.CustomerDAO;
 import com.edutech.progressive.dao.CustomerDAOImpl;
 import com.edutech.progressive.entity.Customers;
 import com.edutech.progressive.service.CustomerService;
 
-public class CustomerServiceImpl implements CustomerService  {
-
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    @Autowired
     private CustomerDAO customerDAO;
-    
-    public CustomerServiceImpl(CustomerDAOImpl customerDAO){
-         this.customerDAO = customerDAO;
+
+    public CustomerServiceImpl(CustomerDAOImpl customerDAO) {
+        this.customerDAO = customerDAO;
     }
 
     @Override
-    public List<Customers> getAllCustomers() throws SQLException {
-        // TODO Auto-generated method stub
+    public List<Customers> getAllCustomers() {
         return customerDAO.getAllCustomers();
     }
 
-    @Override
-    public int addCustomer(Customers customers) throws SQLException {
-        // TODO Auto-generated method stub
-        int generatedId = customerDAO.addCustomer(customers);
-        customers.setCustomerId(generatedId);
-        return generatedId;
+    public Customers getCustomerById(int customerId) {
+        return customerDAO.getCustomerById(customerId);
     }
 
     @Override
-    public List<Customers> getAllCustomersSortedByName() throws SQLException {
-        // TODO Auto-generated method stub
-        List<Customers> list = customerDAO.getAllCustomers();
-        list.sort((a,b) -> a.getName().compareToIgnoreCase(b.getName()));
-        return list;
-     }
+    public int addCustomer(Customers customers) {
+        return customerDAO.addCustomer(customers);
+    }
 
-    public void updateCustomer(Customers customers) throws SQLException{
-            customerDAO.updateCustomer(customers);
+    public void updateCustomer(Customers customers) {
+        customerDAO.updateCustomer(customers);
     }
-    public void deleteCustomer(int customerId)throws SQLException{
-           customerDAO.deleteCustomer(customerId);
+
+    public void deleteCustomer(int customerId) {
+        customerDAO.deleteCustomer(customerId);
     }
-    public Customers getCustomerById(int customerId) throws SQLException{
-          return customerDAO.getCustomerById(customerId);
+
+    @Override
+    public List<Customers> getAllCustomersSortedByName() {
+        List<Customers> ans = customerDAO.getAllCustomers();
+        Collections.sort(ans);
+        return ans;
     }
 
 }
