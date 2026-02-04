f82a10a3850ca2ec2e6fdd5b960c5fc67ae0360b
creativekickstart23 <creativekickstart23@gmail.com>
2026-02-05 01:04:48 +0530

    Update Transactions.java

--- server/src/main/java/com/edutech/progressive/entity/Transactions.java
+++ server/src/main/java/com/edutech/progressive/entity/Transactions.java
@@ -1,54 +1,70 @@
 package com.edutech.progressive.entity;
 
-import java.sql.Timestamp;
 import java.util.Date;
 
+import javax.persistence.*;
+
+@Entity
 public class Transactions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int transactionId;
+    // @ManyToOne
+    // @JoinColumn(name = "accountId")
     private int accountId;
     private double amount;
     private String transactionType;
     private Date transactionDate;
 
-    public Transactions(int i, int j, double d, Timestamp timestamp, String string) {
+    public Transactions() {
     }
 
-    public Transactions(int transactionId, int accountId, double amount, String transactionType, Date transactionDate) {
+    public Transactions(int transactionId, int accountId, double amount, Date transactionDate, String transactionType) {
         this.transactionId = transactionId;
         this.accountId = accountId;
         this.amount = amount;
-        this.transactionType = transactionType;
         this.transactionDate = transactionDate;
+        this.transactionType = transactionType;
     }
 
     public int getTransactionId() {
         return transactionId;
     }
+
     public void setTransactionId(int transactionId) {
         this.transactionId = transactionId;
     }
+
     public int getAccountId() {
         return accountId;
     }
+
     public void setAccountId(int accountId) {
         this.accountId = accountId;
     }
+
     public double getAmount() {
         return amount;
     }
+
     public void setAmount(double amount) {
         this.amount = amount;
     }
+
     public String getTransactionType() {
         return transactionType;
     }
+
     public void setTransactionType(String transactionType) {
         this.transactionType = transactionType;
     }
+
     public Date getTransactionDate() {
         return transactionDate;
     }
+
     public void setTransactionDate(Date transactionDate) {
         this.transactionDate = transactionDate;
     }
+
 }
